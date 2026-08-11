5f5db4bf7f3f722dbec743db4e3f396a998d660a
sena-nana <34237511+sena-nana@users.noreply.github.com>
2026-08-11 19:00:25 +0800

    启用 iced svg 并 vendoring iced_wgpu/winit 补丁。
    svg 使 Batch 变为 Vec，需本地 iced_wgpu 修复；Android 缺少 arboard/modifier_supplement，一并 patch 以保持单一 WGPU 主版本可编译。

---
+++ vendor/iced_winit/src/error.rs
@@ -0,0 +1,24 @@
+use crate::core::backend;
+use crate::futures::futures;
+
+/// An error that occurred while running an application.
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    /// The futures executor could not be created.
+    #[error("the futures executor could not be created")]
+    ExecutorCreationFailed(futures::io::Error),
+
+    /// The application window could not be created.
+    #[error("the application window could not be created")]
+    WindowCreationFailed(winit::error::OsError),
+
+    /// The application graphics context could not be created.
+    #[error("the application graphics context could not be created")]
+    GraphicsCreationFailed(backend::Error),
+}
+
+impl From<backend::Error> for Error {
+    fn from(error: backend::Error) -> Error {
+        Error::GraphicsCreationFailed(error)
+    }
+}
